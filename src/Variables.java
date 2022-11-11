234d11db78f7bd1a1ac11f48720db56a71a61ba5
EVEB555 <97383055+EVEB555@users.noreply.github.com>
2022-11-11 21:14:19 +0200

    added variables class

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        //declaring new variable of type int
+        int number;
+
+        //initialization of number variable
+        number = 10;
+
+        //accessing number variables value
+        System.out.println(number);
+
+        //declaring and initializing new variable at the same time
+        int age = 67;
+        System.out.println(age);
+        System.out.println("age = " + age);
+   }
+}
+
+
+
+

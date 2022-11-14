236d2c6b7213109155294e6c26d46cc3417fc0b9
EVEB555 <97383055+EVEB555@users.noreply.github.com>
2022-11-14 21:34:46 +0200

    operators, variables added

---
+++ src/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+        //Integer Data Type
+        byte num1 = 120;
+        short num2 = 10000;
+        int num3 = 23232323;
+        long num4 = 432125161L;
+
+        //Floating point data type
+        float num5 = 1565.565f;
+        double num6 = 556664666.6655d;
+
+        //Booliean data type
+        boolean redLightSwitch = false;
+        boolean blackLightSwitch = true;
+
+        //Characters
+
+        char char1 = '%';
+        char char2 = 'y';
+        char char3 = '0';
+        char char4 = '\n';
+
+        //String
+
+        String text1 = "This is very good text";
+
+        System.out.println(num2);
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,62 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        //Assignment operators =, +=, -=, *=, /=
+
+        int a = 100;
+        System.out.println("a = " + a);
+
+        a += 10;
+        System.out.println("a = " + a);
+
+        // Basic arithmetic operators
+
+        //23 % 6 = 5 -> 18 telpa (6 po tris) ir lieka 5 (23 - 18)
+
+        //Incrementation and decrementation
+
+        //post incrementation
+        int num1 = 10;
+        num1++;
+        System.out.println("num1 = " + num1);
+        System.out.println("num1 = " + num1++); // first prints than changes
+        System.out.println("num1 = " + num1);
+
+        //pre incrementation/decrimentation
+        System.out.println("num1 = " + --num1);
+        System.out.println("num1 = " + ++num1); //changes and prints
+
+        //4 ways to increase or decrease variable by one
+        num1 += 1;
+        num1 = num1 + 1;
+        num1++;
+        ++num1;
+
+        //relational operators
+
+        //ewuality ==
+        int i = 1;
+        int b = 10;
+        System.out.println("Equality " + (i == b));
+
+        //Inequality !=
+        System.out.println("Inequality " + (i!=b));
+
+        //greater than > and greater than or equal to >=
+        System.out.println("greater than " + (i > b));
+        System.out.println("greater than or equal to " + (i >= b));
+
+        //less than < and less than or equal to
+        System.out.println("greater than " + (i < b));
+        System.out.println("greater than or equal to " + (i <= b));
+
+        //logical operators
+        //logical and - &&
+
+        boolean sun = true;
+        boolean dry = true;
+
+        System.out.println(" I will go outside " + (sun && dry));
+
+    }
+}
